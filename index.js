a4628bc53fe63c92a863b07b96feda97723a5c5e
Dario Vladovic <d.vladimyr@gmail.com>
2019-05-01 01:50:34 +0200

    Change internals, update tests
    - overriding `Sequelize#query` instead of overriding
      `queryInterface` methods one by one
    - applying plugin to connection factory rather than connection
      itself (gets applied to all created connections)
    - escaping comment using `Sequelize#escape` combined with custom
      comment escaping logic
    - refactoring tests to use in-memory sqlite instead of mysql
    - tracking queries by spying on `sqlite3` methods:
      https://github.com/mapbox/node-sqlite3/wiki/API#database
    - introducing proper test suite powered by ava and sinon
    - cleaning up obsolete deps
    - updating package license modifier & published file list

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 'use strict';
 
-module.exports = require('./lib/index');
+const $plugin = Symbol(require('./package.json').name);
+
+module.exports = (Sequelize, { newline = false } = {}) => {
+  if (Sequelize[$plugin]) return;
+  const delimiter = newline ? '\n' : ' ';
+  const { query } = Sequelize.prototype;
+  Sequelize.prototype.query = function(sql, { comment, ...options } = {}) {
+    if (!comment) return query.call(this, sql, options);
+    comment = `/* ${escapeComment(this.escape(comment))} */`;
+    if (typeof sql === 'object') {
+      sql.query = [comment, sql.query].join(delimiter);
+      return query.call(this, sql, options);
+    }
+    sql = [comment, sql].join(delimiter);
+    return query.call(this, sql, options);
+  };
+  Sequelize[$plugin] = true;
+};
+
+function escapeComment(comment) {
+  return comment
+    .replace(/[\n\r]/g, match => {
+      if (match === '\n') return '\\n';
+      if (match === '\r') return '\\r';
+    })
+    .replace(/\/\*|\*\//g, match => {
+      return match.replace(/./g, '\\$&');
+    });
+}
